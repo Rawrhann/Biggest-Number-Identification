c05efc1658f289e94e47ec2bd744270a594bd098
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-06 22:18:56 +0800

    Instructions

---
+++ Identification of the Biggest Number.py
@@ -0,0 +1,7 @@
+
+#Ask user to input 3 numbers. Find and print the biggest num,ber using only if statement
+#create a new github repository
+#Upload your progress to github using git bash
+#Record a demo presenting your program [less than 2 min only]
+#Send the demo directly to sir Danilo's messenger
+#Deadline before Jan 13, 2023
