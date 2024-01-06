505898ae7d7f0c52f2e9e37381a8d8931c56ca33
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-06 23:11:28 +0800

    Pseudocode final

--- identification_of_the_biggest_number.py
+++ identification_of_the_biggest_number.py
@@ -8,4 +8,21 @@
 
 #Pseudo code
 
+#Asking for numbers and checking if it is a number
 
+#Input First number
+#Check if the first number is a number
+
+#Input second number
+#Check if the second number is a number
+
+#Input third number
+#Check if the third number is a number
+
+#Identification of larger numbers
+#Between the first and second number, check which is higher
+#Between the first and third number, check which is higher
+#Between the second and third number, check which is higher
+
+#The number that will get a higher result twice would be the highest between them all
+#Display that number
