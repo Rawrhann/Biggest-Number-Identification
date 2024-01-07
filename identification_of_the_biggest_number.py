5a9f7f28469d5478cb470dbacb7e57cbf3c27595
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-07 14:24:47 +0800

    Not working Display

--- identification_of_the_biggest_number.py
+++ identification_of_the_biggest_number.py
@@ -20,19 +20,26 @@ from tkinter.simpledialog import Dialog
 #Between the first and second number, check which is higher
 #Between the first and third number, check which is higher
 #Between the second and third number, check which is higher
+#The number that will get a higher result twice would be the highest between them all
+#Display that number
 def identify(highest_number):
     if first_number_variable > second_number_variable and first_number_variable > third_number_variable:
-        return highest_number
+        highest_number = first_number_variable
     elif second_number_variable > third_number_variable:
-        return highest_number
+        highest_number = second_number_variable
     else:
-        return highest_number
+        highest_number = third_number_variable
+    
+    highest_number_label = tk.Label(main_window, font=courier_font, bg="#454545", fg="white", relief="solid", justify=LEFT)
+    highest_number_label.configure(text=str (("I am ", (highest_number),  " years old")))
+        
 #Asking for numbers and checking if it is a number
 
 main_window = tk.Tk() 
 main_window.title ("Startup")
 main_window.config(bg = "#212121")
 font.families()
+courier_font = ("courier", 13, 'bold')
 
 # for storing numbers
 first_number_variable=tk.IntVar
@@ -44,39 +51,38 @@ button_exit = Button(main_window, width = "15", height = "2", text = "Exit", com
 
 #Input First number
 first_number_entry = tk.Entry(main_window, textvariable = first_number_variable, font=('calibre',10,'normal'))
-first_number_label = tk.Label(main_window, text = 'First Number', font=('calibre',10, 'bold'), bg="#454545", fg="white", relief="solid")
+first_number_label = tk.Label(main_window, text = 'First Number', font=courier_font, bg="#454545", fg="white", relief="solid", justify="left")
 #Check if the first number is a number
 
 
 #Input second number
 second_number_entry = tk.Entry(main_window, textvariable = second_number_variable, font=('calibre',10,'normal'))
-second_number_label = tk.Label(main_window, text = 'Second Number', font=('calibre',10, 'bold'), bg="#454545", fg="white", relief="solid")
+second_number_label = tk.Label(main_window, text = 'Second Number', font=courier_font, bg="#454545", fg="white", relief="solid", justify="left")
 #Check if the second number is a number
 
 #Input third number
 third_number_entry = tk.Entry(main_window, textvariable = third_number_variable, font=('calibre',10,'normal'))
-third_number_label = tk.Label(main_window, text = 'Third Number', font=('calibre',10, 'bold'), bg="#454545", fg="white", relief="solid")
+third_number_label = tk.Label(main_window, text = 'Third Number', font=courier_font, bg="#454545", fg="white", relief="solid", justify=LEFT)
 #Check if the third number is a number
 
 
 
 
-#The number that will get a higher result twice would be the highest between them all
-#Display that number
+
 
 #Placements in the main window
 button_start.place(x=0, y=0)
-button_exit.place(x=0, y=560)
-first_number_label.place(height=15, width=150, x=100, y=50)
-second_number_label.place(height=15, width=150, x=100, y=150)
-third_number_label.place(height=15, width=150, x=100, y=250)
+button_exit.place(x=0, y=460)
+first_number_label.place(height=30, width=150, x=100, y=50)
+second_number_label.place(height=30, width=150, x=100, y=150)
+third_number_label.place(height=30, width=150, x=100, y=250)
 
-first_number_entry.place(height=15, width=150, x=100, y=100)
-second_number_entry.place(height=15, width=150, x=100, y=200)
-third_number_entry.place(height=15, width=150, x=100, y=300)
+first_number_entry.place(height=30, width=150, x=100, y=100)
+second_number_entry.place(height=30, width=150, x=100, y=200)
+third_number_entry.place(height=30, width=150, x=100, y=300)
 
 #Main window size
-main_window.geometry("400x600")
+main_window.geometry("400x500")
 main_window.resizable(False, False)
 
 main_window.mainloop()
